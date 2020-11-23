0724c19a8bccb838b183241357465793e1c63065
u10210018 <u10210018@go.utaipei.edu.tw>
2020-11-24 01:47:55 +0800

    update main,js import

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-import './assets/css/reset.scss';
+import "./assets/css/reset.scss";
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import VueAwesomeSwiper from "vue-awesome-swiper";
 import swiper, {
-  Navigation,
-  Pagination,
   Autoplay,
   EffectFade
 } from "swiper";
@@ -13,8 +11,8 @@ import "swiper/swiper-bundle.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-Vue.use(VueAwesomeSwiper, /* { default options with global component } */ )
-swiper.use([Navigation, Pagination, Autoplay, EffectFade])
+Vue.use(VueAwesomeSwiper);
+swiper.use([Autoplay, EffectFade]);
 Vue.config.productionTip = false;
 
 new Vue({
